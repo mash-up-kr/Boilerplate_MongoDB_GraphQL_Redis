3cc9532f9f2d500fbf5c550c602da2c8efd9ade2
nari1021 <y.shhh000@gmail.com>
2021-02-02 17:42:54 +0900

    feat: POST addPosts resolvers 작성

--- src/domains/posts/resolvers/addPosts.js
+++ src/domains/posts/resolvers/addPosts.js
@@ -1,17 +1,24 @@
 import {Post} from '../models/index.js';
 
-const addPosts = async (_, {
-  title,
-  content,
-  createdAt,
-}) => {
-  const newPost = await Post.create({
+const addPosts = async (parent, args, context, info) => {
+  const {title, author, content} = args;
+  const session = await Post.startSession();
+  session.startTransaction();
+
+  const newPost = new Post({
     title,
+    author,
     content,
-    createdAt,
   });
 
-  return newPost;
+  const result = [];
+  result.push(await newPost.save({session}));
+
+  await session.commitTransaction();
+  session.endSession();
+
+  console.log(newPost);
+  return result;
 };
 
 export default addPosts;
