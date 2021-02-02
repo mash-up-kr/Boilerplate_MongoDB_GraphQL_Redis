aa99b4c2998ec238f4c7d084c56a2e81fa037cd8
nari1021 <y.shhh000@gmail.com>
2021-02-02 19:28:28 +0900

    feat: POST editPost resolvers 작성

--- src/domains/posts/resolvers/editPost.js
+++ src/domains/posts/resolvers/editPost.js
@@ -1,15 +1,18 @@
-const editPost = async (_, {
-  id,
-  title,
-  author,
-  content,
-}) => {
-  const editPost = {
-    id,
-    title,
-    author,
-    content,
-  };
+import {Post} from '../models/index.js';
+
+const editPost = async (parent, args, context, info) => {
+  const {_id, title, author, content} = args;
+
+  const editPost = await Post.findByIdAndUpdate(
+      _id,
+      {title,
+        author,
+        content,
+      },
+      {new: true,
+        upsert: true,
+      },
+  );
 
   return editPost;
 };
