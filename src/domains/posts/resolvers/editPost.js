6571c755045778b66834c99bac7f178b0f0e41e3
nari1021 <y.shhh000@gmail.com>
2021-02-05 20:34:34 +0900

    feat: POST editPost resolvers 수정

--- src/domains/posts/resolvers/editPost.js
+++ src/domains/posts/resolvers/editPost.js
@@ -3,15 +3,21 @@ import {Post} from '../models/index.js';
 const editPost = async (parent, args, context, info) => {
   const {_id, title, author, content} = args;
 
-  const editPost = await Post.findByIdAndUpdate(
-      _id,
-      {title,
-        author,
-        content,
-      },
-      {new: true,
-        upsert: true,
-      },
+
+  const filter = {
+    _id,
+  };
+
+  const update = {
+    title,
+    author,
+    content,
+  };
+
+  const editPost = await Post.findOneAndUpdate(
+      filter,
+      update,
+      {new: true},
   );
 
   return editPost;
